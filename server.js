417d6e8081fbc9c3d867925b319538478dbb6e40
Pcjos <65132034+Pcjos@users.noreply.github.com>
2020-05-13 22:41:07 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,51 @@
+var http = require('http');  
+var url = require('url');  
+var fs = require('fs');  
+var server = http.createServer(function(request, response) {  
+    var path = url.parse(request.url).pathname;  
+    switch (path) {  
+        case '/':  
+            response.writeHead(200, {  
+                'Content-Type': 'text/plain'  
+            });  
+            response.write("This is Test Message.");  
+            response.end();  
+            break;  
+        case '/page1.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+            break;  
+        case '/page2.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+            break;  
+        default:  
+            response.writeHead(404);  
+            response.write("opps this doesn't exist - 404");  
+            response.end();  
+            break;  
+    }  
+});   
+server.listen(80); 
